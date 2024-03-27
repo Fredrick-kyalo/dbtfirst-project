79914036df18c57821edbbcfcecd9633f8fb4554
Fredrick-kyalo <kyalomwendwa44@gmail.com>
2024-03-27 10:32:24 +0300

    postgres_sample_data

---
+++ models/postgres_sample_data/postgres_Shopdb.sql
@@ -0,0 +1,11 @@
+--Common table experessions
+
+with shopdb_customers as(
+    select * from {{ source('Shopdb', 'customer') }}
+),
+
+final as(
+    select * from shopdb_customers
+)
+
+select * from final
